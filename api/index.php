6d355d3918d09ff61d4f09d3f5c1b50c39029e71
adindaaasals <adindaaasals28@gmail.com>
2024-12-21 17:34:25 +0700

    update for deployment

--- api/index.php
+++ api/index.php
@@ -1,3 +1,7 @@
 <?php
 
+// Set LD_LIBRARY_PATH untuk memastikan pustaka yang diperlukan dapat ditemukan
+putenv("LD_LIBRARY_PATH=" . __DIR__ . "/lib");
+
+// Memuat aplikasi utama
 require __DIR__ . '/../public/index.php';
