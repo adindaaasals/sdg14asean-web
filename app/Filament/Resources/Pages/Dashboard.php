7f8685e81c9c7f21f98a14f8806f68c2c06b1ac4
adindaaasals <adindaaasals28@gmail.com>
2024-12-21 16:37:21 +0700

    Update for deployment

--- app/Filament/Resources/Pages/Dashboard.php
+++ app/Filament/Resources/Pages/Dashboard.php
@@ -1,19 +1,19 @@
 <?php
 
-namespace App\Filament\Pages;
+// namespace App\Filament\Pages;
 
-use Filament\Pages\Dashboard as BaseDashboard;
+// use Filament\Pages\Dashboard as BaseDashboard;
 
-class Dashboard extends BaseDashboard
-{
-    public function getWidgets(): array
-    {
-        return [
-            \App\Filament\Widgets\TotalCountries::class,
-            \App\Filament\Resources\TotalFisheriesProductionResource\Widgets\TotalFisheriesProduction::class,
-            \App\Filament\Resources\PagesResource\Widgets\LatestUpdates::class,
-            // \App\Filament\Resources\AquacultureProductionResource\Widgets\AquacultureTrend::class,
-            \App\Filament\Resources\TotalFisheriesProductionResource\Widgets\TopContributorsFisheries::class,
-        ];
-    }
-}
+// class Dashboard extends BaseDashboard
+// {
+//     public function getWidgets(): array
+//     {
+//         return [
+//             \App\Filament\Widgets\TotalCountries::class,
+//             \App\Filament\Resources\TotalFisheriesProductionResource\Widgets\TotalFisheriesProduction::class,
+//             \App\Filament\Resources\PagesResource\Widgets\LatestUpdates::class,
+//             // \App\Filament\Resources\AquacultureProductionResource\Widgets\AquacultureTrend::class,
+//             \App\Filament\Resources\TotalFisheriesProductionResource\Widgets\TopContributorsFisheries::class,
+//         ];
+//     }
+// }
